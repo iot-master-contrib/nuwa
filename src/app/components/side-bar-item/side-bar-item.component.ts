be541df7f09e93fed897091347c9b4335fd76a68
jason <jason@zgwit.com>
2024-05-03 00:45:19 +0800

    支持缩放了，没设最小宽度

--- src/app/components/side-bar-item/side-bar-item.component.ts
+++ src/app/components/side-bar-item/side-bar-item.component.ts
@@ -1,13 +1,17 @@
 import {Component, Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-side-bar-item',
-  standalone: true,
-  imports: [],
-  templateUrl: './side-bar-item.component.html',
-  styleUrl: './side-bar-item.component.scss'
+    selector: 'app-side-bar-item',
+    standalone: true,
+    imports: [
+        CommonModule,
+    ],
+    templateUrl: './side-bar-item.component.html',
+    styleUrl: './side-bar-item.component.scss'
 })
 export class SideBarItemComponent {
-    @Input() Title: string = 'tab'
+    @Input() Title = 'tab'
+    @Input() Activated = false
 
 }
--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ContentChildren, HostBinding, Input, QueryList, ViewChildren} from '@angular/core';
+import {Component, ContentChildren, HostBinding, HostListener, Input, QueryList, ViewChildren} from '@angular/core';
 import {SideBarItemComponent} from "../side-bar-item/side-bar-item.component";
 import {CommonModule} from "@angular/common";
 
@@ -14,10 +14,65 @@ import {CommonModule} from "@angular/common";
 export class SideBarComponent {
     @ContentChildren(SideBarItemComponent, {descendants: true}) components!: QueryList<SideBarItemComponent>
 
-    @HostBinding("style.flex-direction") direction = "row";
+    //右对齐
+    _right = false;
 
-    @Input() set position(p: string) {
-        this.direction = p == "right" ? "row-reverse" : "row";
+    @Input() set Right(p: boolean) {
+        this._right = p
+        this.direction = p ? "row-reverse" : "row";
     }
 
+    @HostBinding("style.flex-direction") direction = "row";
+
+    //宽度
+    _width = 200
+
+    @Input() set Width(w: number) {
+        this._width = w
+        this.width = w + "px"
+    }
+
+    @HostBinding("style.width") width = "200px";
+
+
+    last = -1;
+
+    onClick(i: number) {
+        console.log("side bar on click", i)
+        if (this.last > -1) {
+            // @ts-ignore
+            this.components.get(this.last).Activated = false
+        }
+        if (this.last == i) {
+            this.last = -1
+        } else {
+            // @ts-ignore
+            this.components.get(i).Activated = true
+            this.last = i
+        }
+    }
+
+    splitting = false;
+    lastX = 0;
+
+    onSplit($event: MouseEvent) {
+        this.splitting = true
+        this.lastX = $event.screenX
+    }
+
+    @HostListener("window:mousemove", ["$event"])
+    onMouseMove($event: MouseEvent) {
+        if (!this.splitting) return;
+        if (this._right)
+            this.Width = this._width - $event.screenX + this.lastX;
+        else
+            this.Width = this._width + $event.screenX - this.lastX;
+        this.lastX = $event.screenX
+    }
+
+
+    @HostListener("window:mouseup", ["$event"])
+    onMouseUp($event: MouseEvent) {
+        this.splitting = false
+    }
 }
