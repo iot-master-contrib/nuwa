9fcc0a7700cb5aa1f24f598c91533c6c11f29e43
jason <jason@zgwit.com>
2024-05-08 20:47:09 +0800

    属性优化

--- src/nuwa/widgets/properties.ts
+++ src/nuwa/widgets/properties.ts
@@ -10,9 +10,10 @@ const fontFamilies: SmartSelectOption[] = [
 ]
 
 const fontWeights: SmartSelectOption[] = [
+    {value: "lighter", label: "细"},
     {value: "normal", label: "正常"},
-    {value: "bold", label: "加粗"},
-    //{value: "bolder", label: "加粗+"},
+    {value: "bold", label: "粗"},
+    {value: "bolder", label: "加粗"},
 ]
 
 const fontStyles: SmartSelectOption[] = [
@@ -24,15 +25,22 @@ export function StrokeProperties(id: string): SmartField[] {
     return [
         {label: "填充", key: `attrs/${id}/fill`, type: "color", clear: true},
         {label: "边框颜色", key: `attrs/${id}/stroke`, type: "color", clear: true},
-        {label: "边框大小", key: `attrs/${id}/stroke-width`, type: "number", min: 0, max: 65535},
+        {label: "边框大小", key: `attrs/${id}/strokeWidth`, type: "number", min: 0, max: 65535},
+        {
+            label: "边框线形", key: `attrs/${id}/strokeDasharray`, type: "select", default: '', options: [
+                {value: "", label: "实线"},
+                {value: "5 5", label: "虚线"},
+                {value: "10 10", label: "虚线2"},
+            ]
+        },
     ]
 }
 
 export const TextProperties: SmartField[] = [
-    {label: "文本", key: "attrs/text/text", type: "text"},
+    {label: "文本", key: "attrs/text/text", type: "text", default: "normal"},
     {label: "文本颜色", key: "attrs/text/fill", type: "color", clear: true},
     {label: "字号", key: "attrs/text/fontSize", type: "number", min: 0, max: 65535},
     {label: "字体", key: "attrs/text/fontFamily", type: "select", options: fontFamilies},
     {label: "加粗", key: "attrs/text/fontWeight", type: "select", options: fontWeights},
-    {label: "风格", key: "attrs/text/fontStyle", type: "select", options: fontStyles},
+    {label: "风格", key: "attrs/text/fontStyle", type: "select", options: fontStyles, default: "normal"},
 ]
