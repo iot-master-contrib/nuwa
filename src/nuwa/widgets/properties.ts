08fcf8d5b9b0439a0c343397ca16d1d78c89de0b
jason <jason@zgwit.com>
2024-05-08 18:59:10 +0800

    颜色可以清空了

--- src/nuwa/widgets/properties.ts
+++ src/nuwa/widgets/properties.ts
@@ -3,8 +3,10 @@ import {SmartField, SmartSelectOption} from "iot-master-smart";
 const fontFamilies: SmartSelectOption[] = [
     {value: "SimHei", label: "黑体"},
     {value: "SimSun", label: "宋体"},
-    {value: "SimSun", label: "仿宋"},
+    {value: "FangSong", label: "仿宋"},
     {value: "KaiTi", label: "楷体"},
+    {value: "LiSu", label: "隶书"},
+    {value: "YouYuan", label: "幼圆"},
 ]
 
 const fontWeights: SmartSelectOption[] = [
@@ -18,18 +20,17 @@ const fontStyles: SmartSelectOption[] = [
     {value: "italic", label: "斜体"},
 ]
 
-
 export function StrokeProperties(id: string): SmartField[] {
     return [
-        {label: "填充", key: `attrs/${id}/fill`, type: "color"},
-        {label: "边框颜色", key: `attrs/${id}/stroke`, type: "color"},
+        {label: "填充", key: `attrs/${id}/fill`, type: "color", clear: true},
+        {label: "边框颜色", key: `attrs/${id}/stroke`, type: "color", clear: true},
         {label: "边框大小", key: `attrs/${id}/stroke-width`, type: "number", min: 0, max: 65535},
     ]
 }
 
 export const TextProperties: SmartField[] = [
     {label: "文本", key: "attrs/text/text", type: "text"},
-    {label: "文本颜色", key: "attrs/text/fill", type: "color"},
+    {label: "文本颜色", key: "attrs/text/fill", type: "color", clear: true},
     {label: "字号", key: "attrs/text/fontSize", type: "number", min: 0, max: 65535},
     {label: "字体", key: "attrs/text/fontFamily", type: "select", options: fontFamilies},
     {label: "加粗", key: "attrs/text/fontWeight", type: "select", options: fontWeights},
