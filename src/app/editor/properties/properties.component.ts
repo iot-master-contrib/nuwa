5cb749538d008e8e1fce80cb80bcfb4eb1d12a67
jason <jason@zgwit.com>
2024-05-20 14:18:54 +0800

    解决未监听的问题

--- src/app/editor/properties/properties.component.ts
+++ src/app/editor/properties/properties.component.ts
@@ -77,25 +77,21 @@ export class PropertiesComponent implements OnDestroy, OnInit {
                 data[f.key] = val
         })
         this.data = data
-
-        this.ngAfterViewInit()
     }
 
-    ngAfterViewInit() {
-        setTimeout(() => {
-            this.editor.group.valueChanges.subscribe(res => {
-                console.log("properties change", res)
-                //Object.assign(this.page, res)
-                Object.keys(res).forEach(key => {
-                    this.cell?.setPropByPath(key, res[key])
-                })
-            })
-        }, 100)
-    }
+    // ngAfterViewInit() {
+    //     setTimeout(() => {
+    //         this.editor.group.valueChanges.subscribe(res => {
+    //             console.log("properties change", res)
+    //             //Object.assign(this.page, res)
+    //             Object.keys(res).forEach(key => {
+    //                 this.cell?.setPropByPath(key, res[key])
+    //             })
+    //         })
+    //     }, 100)
+    // }
 
     onChange() {
-        //TODO 未生效，原因不详。page.component.ts中是OK的
-
         let value = this.editor.value
         console.log("properties onChange", value)
         Object.keys(value).forEach(key => {
